985d8b34792c8150d34df1a97b89ff6bb4353b8f
Boris Kováč <Boris Kov>
2016-08-08 20:00:53 +0200

    added basic routes, handlebars and dummy content

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+var express = require("express");
+var handlebars = require('express-handlebars');
+var app = express();
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+/* ROUTES */
+
+// homepage
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+// public
+app.use('/public', express.static(__dirname + '/public'));
+
+
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
