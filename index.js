def20b9c4c40a5c404f2dcc2907343730442a1f3
Boris Kováč <Boris Kov>
2016-08-09 09:50:30 +0200

    upgraded to serve-static

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var handlebars = require('express-handlebars');
+var ss = require('serve-static');
 var app = express();
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
@@ -7,13 +8,17 @@ app.set('view engine', 'handlebars');
 
 /* ROUTES */
 
+// public
+app.use(ss(__dirname + '/public'));
+
+
 // homepage
 app.get('/', function (req, res) {
     res.render('home');
 });
 
-// public
-app.use('/public', express.static(__dirname + '/public'));
+
+
 
 
 
