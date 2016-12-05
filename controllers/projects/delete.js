ee9d19749acdda673a8985080be5bea3df0091e9
bokova <boro.kovac@gmail.com>
2016-12-05 19:45:42 +0100

    Delete controller (#83)

--- controllers/projects/delete.js
+++ controllers/projects/delete.js
@@ -1,8 +1,39 @@
 'use strict'
 
+var db = require('../../database.js');
+var middleware = require('../../middleware/authentication.js');
+
 module.exports = function(app) {
 
-	app.get('/projects/delete/:id', function(req, res) {
-		res.send('Not done yet.');
+	app.get('/projects/delete/:id', middleware.requireAuthentication, function(req, res) {
+		var id = req.params.id;
+		db.transaction(function(trx) {
+			// Note: make sure an address can be assigned to 1 project only or that no other project uses it
+			db('project_addresses')
+				.transacting(trx)
+				.where('project_id', id)
+				.del()
+				.catch(function(error) {
+					console.log(error);
+				})
+				.then(
+					db('projects')
+					.where('id', id)
+					.del()
+					.catch(function(error) {
+						console.log(error);
+					})
+				)
+				.then(trx.commit)
+				.catch(trx.rollback)
+				.catch(function(error) {
+					console.log(error);
+				})
+				.finally(function() {
+					res.redirect('../../')
+
+				})
+		})
+
 	});
 };
