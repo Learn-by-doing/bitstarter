64046235b692e62d81bd4df39672b4cb4fbc2dbb
Charles Hill <chill@degreesofzero.com>
2016-12-09 18:28:50 +0100

    Fixes for travis

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 module.exports = {
 
 	dbOptions : {
-		host     : process.env.BITSTARTER_HOST || '127.0.0.1',
-		port     : process.env.BITSTARTER_PORT || 3306,
-		user     : process.env.BITSTARTER_USER || 'bitstarter',
-		password : process.env.BITSTARTER_PASSWORD || 'password',
-		database : process.env.BITSTARTER_DATABASE || 'bitstarter'
+		host: process.env.BITSTARTER_DB_HOST || '127.0.0.1',
+		port: parseInt(process.env.BITSTARTER_DB_PORT || 3306),
+		user: process.env.BITSTARTER_DB_USER || 'bitstarter',
+		password: process.env.BITSTARTER_DB_PASSWORD || 'password',
+		database: process.env.BITSTARTER_DB_DATABASE || 'bitstarter'
 	}
 };
