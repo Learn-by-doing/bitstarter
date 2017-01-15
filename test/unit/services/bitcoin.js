79af51aaee398eef30010b6e8de5569e5a5291a4
Charles Hill <chill@degreesofzero.com>
2017-01-15 16:51:15 +0100

    Reduce bitcoin service tests to one, add comments

--- test/unit/services/bitcoin.js
+++ test/unit/services/bitcoin.js
@@ -3,33 +3,24 @@
 var bitcoin = require('../../../services/bitcoin');
 var expect = require('chai').expect;
 
-var address1 = '1BHPGY7Rb9WaBBkYPKjZTnKYRzt5mC8NPM';
-var address2 = '1Nkk6rPhFk5UbbNWT12QyCTosgjV8MWjeg';
-var address3 = '16TAy1uNFNEw58uPN4c7C8VnJUYp2ZJUjd';
-
 describe('testing bitcoin.js', function() {
-	this.timeout(30000);
 
 	it('testing function getFunds for 3 addresses', function(done) {
 
-		var address_test = [address1, address2, address3];
-		bitcoin.getFunds(address_test, function(allAccountsReceived) {
+		var testAddresses = [
+			'1BHPGY7Rb9WaBBkYPKjZTnKYRzt5mC8NPM',
+			'1Nkk6rPhFk5UbbNWT12QyCTosgjV8MWjeg',
+			'16TAy1uNFNEw58uPN4c7C8VnJUYp2ZJUjd'
+		];
 
-			expect(allAccountsReceived).to.not.be.undefined;
-			expect(allAccountsReceived).to.be.a('number');
+		// bitcoin.getFunds makes a request to a remote service.
+		// This service is slow to respond sometimes.
+		this.timeout(30000);
+
+		bitcoin.getFunds(testAddresses, function(amount) {
+			expect(amount).to.not.be.undefined;
+			expect(amount).to.be.a('number');
 			done();
 		});
 	});
-
-	it('testing function getFunds for 2 addresses', function(done) {
-
-		var address_test = [address1, address2];
-		bitcoin.getFunds(address_test, function(allAccountsReceived) {
-
-			expect(allAccountsReceived).to.not.be.undefined;
-			expect(allAccountsReceived).to.be.a('number');
-			done();
-		});
-	});
-
 });
